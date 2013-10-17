b31a9286caa0477adb9cbd7bc0ee3c696247245d
Robert Metzger <metzgerr@web.de>
2013-10-17 22:36:35 +0200

    fixed quickstart for real hipster urls

--- quickstart.sh
+++ quickstart.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-
 mvn archetype:generate								\
   -DarchetypeGroupId=eu.stratosphere 				\
   -DarchetypeArtifactId=stratosphere-quickstart		\
   -DarchetypeVersion=0.4-SNAPSHOT					\
-  -DarchetypeCatalog=https://oss.sonatype.org/content/repositories/snapshots/archetype-catalog.xml
+  -DgroupId=eu.stratosphere 						\
+  -DartifactId=quickstart 							\
+  -Dversion=0.1										\
+  -Dpackage=quickstart 								\
+  -DinteractiveMode=false							\
+  -DarchetypeCatalog=https://oss.sonatype.org/content/repositories/snapshots/
+
+
+# Use this command if you want to specify the coordinates of your generated artifact
+# in an interactive menu:
+#
+# mvn archetype:generate								\
+#   -DarchetypeGroupId=eu.stratosphere 				\
+#   -DarchetypeArtifactId=stratosphere-quickstart		\
+#   -DarchetypeVersion=0.4-SNAPSHOT					\
+#   -DarchetypeCatalog=https://oss.sonatype.org/content/repositories/snapshots/
